97f3644ccad16c63710c889771e291735043a7b4
xuxc <690836268@qq.com>
2024-07-18 19:21:53 +0800

    表单input框封装

---
+++ src/components/form/form.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+
+const defaultContext = {
+  model: {},
+  layout: "horizontal",
+  labelAlign: "right",
+  colNum: 1,
+  labelWidth: "1/4",
+};
+
+export const FormContext = React.createContext(defaultContext);
+
+export default function FForm(props) {
+  const contextValue = { ...defaultContext, ...props };
+  return (
+    <FormContext.Provider value={contextValue}>
+      <form className="flex flex-wrap">{props.children}</form>
+    </FormContext.Provider>
+  );
+}
